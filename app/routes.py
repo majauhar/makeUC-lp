6384ddfa237b436f6a599391e8cc07ffa6cfdaa4
Jauhar <siddmohamed19@gmail.com>
2020-10-04 06:24:03 +0530

    added logout option

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
